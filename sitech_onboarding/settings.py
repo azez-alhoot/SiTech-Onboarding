3c431be34014be104ee4f849b4519b31c0d37bc6
reema-eilouti <reema.eilouti@gmail.com>
2021-08-15 14:41:48 +0300

    sanity check using basic template

--- sitech_onboarding/settings.py
+++ sitech_onboarding/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'onboarding.apps.OnboardingConfig',
 ]
 
 MIDDLEWARE = [
