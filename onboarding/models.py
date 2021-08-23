4d574386c8f000134815dff4a951c577d06c0d46
azez-alhoot <azezalhoot82@yahoo.com>
2021-08-23 13:11:19 +0300

    added the sittengs for the media and images upload

--- onboarding/models.py
+++ onboarding/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
 
     title = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='static/user_photos/')
+    image = models.ImageField(upload_to='user_photos')
     
 
     def __str__(self):
